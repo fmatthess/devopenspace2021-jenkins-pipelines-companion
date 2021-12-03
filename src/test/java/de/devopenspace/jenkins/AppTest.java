10ae2c569705ebfc1ea857b08f65a47b740a0ebe
Frank Mattheß <f.matthess@outlook.de>
2021-12-03 14:40:33 +0100

    fixed2

--- src/test/java/de/devopenspace/jenkins/AppTest.java
+++ src/test/java/de/devopenspace/jenkins/AppTest.java
@@ -3,6 +3,7 @@ package de.devopenspace.jenkins;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 /**
  * Unit test for simple App.
